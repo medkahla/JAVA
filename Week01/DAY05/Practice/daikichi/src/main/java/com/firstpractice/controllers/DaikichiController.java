5d53bd57b1f838c1ecf37c4f75fb7cf9f603283d
medkahla <elkahlamed@gmail.com>
2023-08-05 15:03:45 +0100

    Daikichi Path Variables (Updated Practice)

--- Week01/DAY05/Practice/daikichi/src/main/java/com/firstpractice/controllers/DaikichiController.java
+++ Week01/DAY05/Practice/daikichi/src/main/java/com/firstpractice/controllers/DaikichiController.java
@@ -2,6 +2,7 @@ package com.firstpractice.controllers;
 
 import java.util.Iterator;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,21 @@ public class DaikichiController {
 	@RequestMapping("/daikichi/tomorrow")
 	public String daikiTomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/daikichi/travel/{place}")
+	public String daikiTravel(@PathVariable("place") String place) {
+		return "Congratulations! You will soon travel to "+place;
+		
+	}
+	
+	@RequestMapping("/daikichi/lotto/{numb}")
+	public String daikiTravel(@PathVariable("numb") int numb) {
+		if (numb % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		
 	}
 	
 }
