b6205bb888caa9927a30013b0bb72c5dd691ffa4
medkahla <elkahlamed@gmail.com>
2023-08-12 23:23:01 +0100

    Login Registration

---
+++ Week02/DAY05/Core/loginregistration/src/main/java/com/loginregistration/controllers/UserController.java
@@ -0,0 +1,78 @@
+package com.loginregistration.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.loginregistration.models.LoginUser;
+import com.loginregistration.models.User;
+import com.loginregistration.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+public class UserController {
+	
+     @Autowired
+     private UserService userServ;
+    
+    @GetMapping("/")
+    public String index(Model model) {
+        // Bind empty User and LoginUser objects to the JSP
+        // to capture the form input
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newLogin", new LoginUser());
+        return "index.jsp";
+    }
+    
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, 
+            BindingResult result, Model model, HttpSession session) {
+    	
+    	userServ.register(newUser, result);
+        if(result.hasErrors()) {
+            model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        }else {
+        	session.setAttribute("user_id", newUser.getId());
+        	return "redirect:/home";
+        }        
+    }
+    
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+            BindingResult result, Model model, HttpSession session) {
+
+         User user = userServ.login(newLogin, result);
+    
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "index.jsp";
+        }
+ 
+        session.setAttribute("user_id", user.getId());
+        return "redirect:/home";
+    }
+    
+    @GetMapping("/logout")
+    public String logout(HttpSession s) {
+    	s.invalidate();
+    	return "redirect:/";   	
+    }
+    
+    @GetMapping("/home")
+    public String homePage(HttpSession session) {
+    	Long userId = (Long) session.getAttribute("user_id");
+    	if(userId == null) {
+    		return "redirect:/";
+    	}else {
+    		return "home.jsp";
+    	}
+    		
+    }
+}
