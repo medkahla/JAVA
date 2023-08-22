4880159cf092a22b0e9c84c3b14f0ab89b80c0db
medkahla <elkahlamed@gmail.com>
2023-08-22 17:48:51 +0100

    Java Project

---
+++ ConstrucTrac/BackEnd/construcTrac-Back/src/main/java/com/construcTrac/ConstrucTracBackApplication.java
@@ -0,0 +1,13 @@
+package com.construcTrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstrucTracBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstrucTracBackApplication.class, args);
+	}
+
+}
---
+++ ConstrucTrac/BackEnd/construcTrac-Back/src/test/java/com/construcTrac/ConstrucTracBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construcTrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstrucTracBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
